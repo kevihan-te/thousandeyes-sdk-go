fef4473e350c60710591d1ce094fd3c6b3a72bc3
Kevin <kevihan@thousandeyes.com>
2025-03-14 14:08:19 +0000

    + add a manual workflow to release sdk and update go.mod file + update the configuration field to dynamically parse sdk version from the go.mod

--- client/configuration.go
+++ client/configuration.go
@@ -2,13 +2,18 @@ package client
 
 import (
 	"context"
+	"fmt"
+	"golang.org/x/mod/modfile"
+	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
 // Configuration stores the configuration of the API client
 type Configuration struct {
-	UserAgent  string `json:"userAgent,omitempty"`
-	Debug      bool   `json:"debug,omitempty"`
+	userAgent  string
+	Debug      bool `json:"debug,omitempty"`
 	ServerURL  string
 	AuthToken  string
 	HTTPClient *http.Client
@@ -18,11 +23,26 @@ type Configuration struct {
 // NewConfiguration returns a new Configuration object
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
-		UserAgent: "ThousandEyes Go SDK v3",
+		userAgent: fmt.Sprintf("ThousandEyesSDK-Go/%s", getCurrentModuleVersion()),
 		Debug:     false,
 		ServerURL: "https://api.thousandeyes.com/v7",
 	}
 	return cfg
+}
+
+func getCurrentModuleVersion() string {
+	data, err := os.ReadFile("go.mod")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	modFile, err := modfile.Parse("go.mod", data, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	parts := strings.Split(modFile.Module.Mod.Path, "/")
+	return parts[len(parts)-1]
 }
 
 func (c *Configuration) WithAuthToken(authToken string) *Configuration {
